3e157b2022d95904fe5deccba8663debc0141bf0
Tom Matthews <trcm@pm.me>
2020-06-05 11:40:33 +0100

    Determine devices automatically, parsing the XML directly
      - traverse the ioreg tree for up to five devices reporting battery levels
      - parse the XML directly using xmllint rather than awk
      - use an alert box rather than a dialog box
      - some trivial shell changes

--- MagicPowerAlert.sh
+++ MagicPowerAlert.sh
@@ -3,72 +3,69 @@
 # 1. Save locally, chmod +x <file>
 #
 # 2. Add a cron entry to run it, for example:
-#   30 9 * * * /Users/dougb/MagicPower/MagicMousePower.sh
+#   30 9 * * * /Users/dougb/MagicPower/MagicPowerAlert.sh
 #
 
 # You can change the threshold
-THRESHOLD=20
+THRESHOLD=25
 
 # You can change the message, if coffee is not your thing
 MESSAGE="Get a coffee and charge:\n"
 
-# Probably best leave this as is
-DEVICES=("Magic Mouse 2" "Magic Keyboard")
+# Gather the first 5 device names which report a 'BatteryPercent'
+declare -a DEVICES
+for findmagicitem in 1 2 3 4 5; do
+    if name=$(/usr/sbin/ioreg -r -a -k BatteryPercent | /usr/bin/xmllint --xpath "
+                /plist/
+                array/
+                    dict[$findmagicitem]/
+                    key[.='Product']/
+                        following-sibling::*[1]/
+                        text()" - 2>/dev/null); then
+            DEVICES+=("$name")
+    fi
+done
 
 # Change nothing below here
 messages=()
 
-for index in ${!DEVICES[*]}
-do
-        device=${DEVICES[$index]}
-                                                                                     # trying to make it readable
-        powerValue=$(/usr/sbin/ioreg -r -l -n AppleHSBluetoothDevice -a              \
-                | awk -v dev="$device"                                               \
-                        'BEGIN { battery_value = 0 }                                 \
-                         /\<key\>BatteryPercent\<\/key\>/                            \
-                                {                                                    \
-                                        getline;                                     \
-                                        match($0, "[0-9]{1,3}");                     \
-                                        battery_value = substr($0, RSTART, RLENGTH)  \
-                                 }                                                   \
-                         /\<key\>Product\<\/key\>/                                   \
-                                {                                                    \
-                                        getline;                                     \
-                                        if ($0 ~ dev) {                              \
-                                                print battery_value;                 \
-                                                exit 0;                              \
-                                        } else {                                     \
-                                                battery_value = 0                    \
-                                        }                                            \
-                                }                                                    \
-                         ')
-
-	int_re='^[0-9]+$'
-	if [[ $powerValue =~ $int_re ]] ; then
-        	if [ $powerValue -le $THRESHOLD ]; then
-			messages[$index]="$device at $powerValue%"
-        	fi
-	fi
+for index in ${!DEVICES[*]}; do
+    device=${DEVICES[$index]}
+    powerValue=$(/usr/sbin/ioreg -r -a -k BatteryPercent 2>&1 | /usr/bin/xmllint --xpath "
+                    /plist/
+                      array/
+                        dict/
+                          key[.='Product']/
+                            following-sibling::*[1][text()=\"$device\"]/
+                          ../
+                            key[.='BatteryPercent']/
+                              following-sibling::*[1]/
+                                text()" - 2>/dev/null)
+    int_re='^[0-9]+$'
+    if [[ $powerValue =~ $int_re ]] ; then
+        if [[ $powerValue -le $THRESHOLD ]]; then
+            messages[$index]="$device at $powerValue%"
+        fi
+    fi
 done
 
 len=${#messages[@]}
 if (( "$len" > 0 )); then
 
-        if [[ -z $MESSAGE ]]; then
-                message="Get a coffee and charge:\n"
-	else
-                message=$MESSAGE
-        fi
+    if [[ -z $MESSAGE ]]; then
+            message="Get a coffee and charge:\n"
+    else
+            message=$MESSAGE
+    fi
 
-        for index in ${!messages[*]}
-        do
-                message="$message\n\t${messages[$index]}"
-        done
+    for index in ${!messages[*]}; do
+        message="$message\n\t${messages[$index]}"
+    done
 
-        /usr/bin/osascript <<-EOF
-           tell application "System Events"
-              activate
-             display dialog "$message"
-           end tell
-	EOF
+    /usr/bin/osascript -e "
+        tell application \"System Events\"
+            activate
+            display alert \"$message\"
+        end tell
+    " >/dev/null 2>&1
 fi
